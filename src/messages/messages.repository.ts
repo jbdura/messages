91cc06f55d46c1253c90a1460b83ab275c7b731a
jbdura <bidurajohnson@gmail.com>
2023-03-11 09:48:52 +0300

    done that level

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,5 +1,7 @@
+import { Injectable } from "@nestjs/common";
 import { readFile, writeFile } from "fs/promises";
 
+@Injectable()
 export class MessagesRepository {
     async findOne(id: string) {
         const contents = await readFile('messages.json', 'utf8');
@@ -9,10 +11,20 @@ export class MessagesRepository {
     }
 
     async findAll() {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
 
+        return messages;
     }
 
-    async create(message: string) {
-        
+    async create(content: string) {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+
+        const id = Math.floor(Math.random() * 999)
+
+        messages[id] = {id, content}
+
+        await writeFile('messages.json', JSON.stringify(messages));
     }
 }
